a328259d364160b55c6b70e4d9bc6ca28ec9cd24
mlprichard <41245161+mlprichard@users.noreply.github.com>
2020-04-16 18:35:31 -0700

    Adding Bloch wave function
    Quick update

--- ComputeBandStruc.m
+++ ComputeBandStruc.m
@@ -42,7 +42,7 @@ end
 %since these are effectively indices (in fourier space), we need these to
 %be rounded to integers. They pretty much already are to MATLAB precision
 deltaKsUnitless = round(deltaKsUnitless);
-potentialDepth = 100; %in Er!!
+potentialDepth = 5; %in Er!!
 waveAmplitudes = waveAmplitudes.*(potentialDepth./maxAmp);
 %% Find the Complex Exponential Coefficients
 %Effectively I just want the coefficients of the complex fourier series
@@ -69,6 +69,8 @@ for jj = 1:length(waveAmplitudes)
     Vcoeff(xKcomp+(max_m+1),yKcomp+(max_m+1)) = Vcoeff(xKcomp+(max_m+1),yKcomp+(max_m+1)) + waveAmplitudes(jj).*(exp(-1i*deltaPhis(jj)))./2;
     Vcoeff(-xKcomp+(max_m+1),-yKcomp+(max_m+1)) = Vcoeff(-xKcomp+(max_m+1),-yKcomp+(max_m+1)) + waveAmplitudes(jj).*(exp(1i*deltaPhis(jj)))./2;
 end
+%I have added this here since I think that Peter's code outputs the 
+Vcoeff = -Vcoeff;
 %% Explicitly Create the Hamiltonian as a Matrix
 % this may look simple yet confusing. There is a bit going on here.
 % Hopefully the document can help explain
@@ -216,6 +218,49 @@ for ii = 1:(2*max_m+1)
         my = myMat(ii,jj);
         U = U + weights(jj,ii).*(exp(1i.*2.*pi.*mx.*xmat)).*(exp(1i.*2.*pi.*my.*ymat));
     end
+end
+
+end
+
+
+function [U] = bloch_wave(eigvecs,max_m,xmat,ymat,quasiX,quasiY,bands)
+%Now the goal of this function is to actually return the eigenfunctions of
+%the Hamiltonian. The other ones are the bloch functions (that have the
+%periodicity of the lattice)
+
+%This is the grid of higher momenta in general.
+[mxMat,myMat] = meshgrid(-max_m:max_m,-max_m:max_m);
+mLength = 2*max_m + 1;
+%Note that the first two indices are the real space representation of the
+%bloch waves. The next two indices are the quasimomenta. The final one is
+%the band index. I think that we should only need the first four, since we
+%have four minima in a single lattice cell
+U = zeros(length(xmat(:,1)),length(ymat(:,1)),length(bands),length(quasiX(:,1)),length(quasiY(:,1)));
+for kk = bands
+    %please see the note in the bloch function function about why this is
+    %weightsMatrix(jj,ii) as opposed to (ii,jj).
+    for ll = 1:length(quasiX(:,1))
+        for mm = 1:length(quasiY(:,1))
+            
+            weightsColumn  = eigvecs(:,kk,ll,mm);
+            weightsMatrix = zeros(mLength,mLength);
+            for ii = 1:mLength
+                for jj = 1:mLength
+                    weightsMatrix(ii,jj) = weightsColumn((mLength*(ii-1)+jj));
+                end
+            end
+            
+            for ii = 1:mLength
+                for jj = 1:mLength
+                    mx = mxMat(ii,jj);
+                    my = myMat(ii,jj);
+                    U(:,:,kk,ll,mm) = U(:,:,kk,ll,mm) +...
+                        weightsMatrix(jj,ii).*(exp(1i.*2.*pi.*(mx+quasiX(ll,mm)).*xmat)).*(exp(1i.*2.*pi.*(my+quasiY(ll,mm)).*ymat));
+                end
+            end
+        end
+    end
+    
 end
 
 end
